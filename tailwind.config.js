c215990163abb2b10b231797916fb0f730d4a205
sabyasachi-arya <73552887+sabyasachi-arya@users.noreply.github.com>
2026-05-22 15:56:36 +0400

    Add Montserrat font to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
       colors: {
         primary: {
           50: '#eff6ff',

c9ba430b23a2d770f0b658223bc8d9632cf84274
sabyasachi-arya <73552887+sabyasachi-arya@users.noreply.github.com>
2026-05-23 14:40:19 +0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,11 +44,11 @@ const Footer = () => {
             </p>
             <div className="space-y-3">
               <div className="flex items-center text-white/80">
-                <PhoneIcon className="w-5 h-5 mr-3 text-emerald" />
+                <PhoneIcon className="w-5 h-5 mr-3 text-white" />
                 <span>+971 54 403 4567</span>
               </div>
               <div className="flex items-center text-white/80">
-                <EnvelopeIcon className="w-5 h-5 mr-3 text-emerald" />
+                <EnvelopeIcon className="w-5 h-5 mr-3 text-white" />
                 <span>Info@ajivacargotrading.com</span>
               </div>
               <div className="flex items-start text-white/80">

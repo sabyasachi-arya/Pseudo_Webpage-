243288df44ef0848ee6fb9d833539781a3c4f652
sabyasachi-arya <73552887+sabyasachi-arya@users.noreply.github.com>
2026-05-25 12:45:57 +0400

    Update MarketingHome.tsx

--- src/pages/MarketingHome.tsx
+++ src/pages/MarketingHome.tsx
@@ -8,12 +8,14 @@ import Contact from '../components/Contact';
 import FAQ from '../components/FAQ';
 import Footer from '../components/Footer';
 import Testimonials from '../components/Testimonials';
+import TrustedPartnerships from '../components/TrustedPartnerships';
 
 export default function MarketingHome() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <TrustedPartnerships />
       <Services />
       <StrategicAdvantage />
       <GCCNetwork />
